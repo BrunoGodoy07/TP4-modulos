7860c5643324bfbd7611b2d7f3a856051aebff0b
Android Studio <devandroid@ort>
2025-05-09 12:25:23 -0300

    TP corregido

--- index.js
+++ index.js
@@ -12,17 +12,29 @@ const alumno1 = new Alumno("Martina", "12345678");
 const alumno2 = new Alumno("Luciano", "87654321");
 
 
-
+console.log("Ejercicio numero 1");
 concatenarString("Bruno","Nehuen")
+console.log("----------------------------------------------");
+console.log("Ejercicio numero 2");
 mostrarCuentas(4,2)
+console.log("----------------------------------------------");
+console.log("Ejercicio numero 3");
 mostrarAlumnos(alumno1, alumno2)
+console.log("----------------------------------------------");
+console.log("Ejercicio numero 4");
 copiar("./entrada.txt", "./salida.txt");
+console.log("----------------------------------------------");
 
+console.log("Ejercicio numero 5");
 let url=parsearUrl("http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo");
 console.log(url)
+console.log("----------------------------------------------");
+console.log("Ejercicio numero 6");
 let ulr2=parsearUrl2("http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo")
 console.log(ulr2)
+console.log("----------------------------------------------");
 
+console.log("Ejercicio numero 7");
 let m1=obtenerMoneda("AR")
 let m2=obtenerMoneda("US")
 let m3=obtenerMoneda("JP")
